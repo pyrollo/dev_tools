12969e6f26dcd228c01ddd929959ee3dd495f217
Pierre-Yves Rollo <dev@pyrollo.com>
2018-12-06 16:07:19 +0100

    New look command

--- init.lua
+++ init.lua
@@ -1,13 +1,29 @@
 local commands = {}
 
+local function pop_word(str)
+	if str == nil then return end
+	local found = string.find(str, " ", 1) or (#str + 1)
+	local word = string.sub(str, 1, found - 1)
+	local remaining = string.sub(str, found + 1)
+	return word, remaining
+end
+
+local function pop_number(str)
+	local word, remaining = pop_word(str)
+	local num = tonumber(word)
+	if num == nil then
+		return nil, str
+	else
+		return num, remaining
+	end
+end
+
 minetest.register_chatcommand("dev", {
 	params = "<command> <arguments>",
 	description = "Launch a dev tools command. Type /dev help for command list.",
 	privs = {},
 	func = function(pname, params)
-		local found = string.find(params, " ", 1) or (#params + 1)
-		local command = string.sub(params, 1, found - 1)
-		local params = string.sub(params, found + 1)
+		local command, params = pop_word(params)
 
 		if command == '' then
 			return commands.help.func(pname, '')
@@ -38,7 +54,8 @@ commands.help = {
 				if not commands[params] then
 					return false, "Unknown command '"..(params or '').."'. Type /dev help for command list."
 				end
-				return true, "/dev "..params.." "..commands[params].params.."\n"..commands[params].description
+				return true, "/dev "..params.." "..commands[params].params.."\n"..
+					(commands[params].long_desc or commands[params].description)
 			end
 		end,
 }
@@ -66,3 +83,43 @@ commands.entities = {
 			return true, count.." entities dumped in log"
 		end,
 }
+
+local looks = {
+	n = 0, e = math.pi, s = 2*math.pi, w = -math.pi,
+	ne = math.pi/2, nw = -math.pi/2, se = 3*math.pi/2, sw = -3*math.pi/2
+}
+
+commands.look = {
+	params = "<h/v/cardinal> [<degrees>]",
+	description = "Set look direction.",
+	long_desc = "Set look direction :\nh <degrees> : horizontal\nv <degrees> : vertical\nn,s,e,w : North/South/East/West (ne, se, nw, sw also usable)",
+	func = function(pname, params)
+		local player = minetest.get_player_by_name(pname)
+		if not player then
+			return false, "Player not found"
+		end
+		local command, params = pop_word(params)
+		if command == '' then
+			return false, "More arguments needed."
+		end
+		command = command:lower()
+		if command == 'h' or command == 'v' then
+			local degrees = pop_number(params)
+			if degrees == nil then
+				return false, "Number of degrees should be given as argument to /dev look "..command
+			end
+			if command == 'h' then
+				player:set_look_horizontal(math.rad(degrees))
+			else
+				player:set_look_vertical(math.rad(degrees))
+			end
+		elseif looks[command] then
+			player:set_look_horizontal(looks[command])
+		else
+			return false, "Unknown '"..command.."' orientation."
+		end
+		return true, "Oriented."
+	end
+}
+
+-- TODO : Pos adjust to corner/center + orientation
